c2829e8fe176fd86ccf367ac3543c675593c3451
Filipe Fonseca <filipedfs@gmail.com>
2021-10-12 06:22:34 -0300

    Initial commit

---
+++ src/test/java/com/filipefonseca/communication/CommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipefonseca.communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
